e1fb5363df899ad29329675fcd84814cd38555be
Jelani <jhankins02@gmail.com>
2018-05-31 23:33:28 -0500

    cleaned up lint errors

--- react-client/src/components/happyHourList.jsx
+++ react-client/src/components/happyHourList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import HappyHourEntry from './happyHourEntry.jsx';
 
-const happyHourList = (props) => (
-    <div>
-        <h4> happy Hour List </h4>
-        There are {props.items.length} Happy Hours Nearby!
+const happyHourList = props => (
+  <div>
+    <h4> happy Hour List </h4>
+      There are {props.items.length} Happy Hours Nearby!
     {props.items.map(item => <HappyHourEntry item={item} />)}
-    </div>
-)
+  </div>
+);
 
 export default happyHourList;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -3,38 +3,40 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import HappyHourList from './components/happyHourList.jsx';
 
+
 console.log('IT WORKED');
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: [{description: 'Happy Hour 1'}]
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [{ description: 'Happy Hour 1' }],
+    };
+  }
 
-    // after the component mounts
-    componentDidMount() {
-        // populate happy Hour list with get request.
-        // $.ajax({
-        //     url: '/happyHours',
-        //     success: (data) => {
-        //         this.setState({
-        //             items: data
-        //         })
-        //     },
-        //     error: (err) => {
-        //         console.log('err', err);
-        //     }
-        // });
-    }
+  // after the component mounts
+  componentDidMount() {
+    // populate happy Hour list with get request.
+    // $.ajax({
+    //     url: '/happyHours',
+    //     success: (data) => {
+    //         this.setState({
+    //             items: data
+    //         })
+    //     },
+    //     error: (err) => {
+    //         console.log('err', err);
+    //     }
+    // });
+  }
 
-    render() {
-        return (<div>
-            <h1>Happy Hour List</h1>
-            <HappyHourList items={this.state.items} />
-        </div>)
-    }
+  render() {
+    return (
+      <div>
+        <h1>Happy Hour List</h1>
+        <HappyHourList items={this.state.items} />
+      </div>);
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
