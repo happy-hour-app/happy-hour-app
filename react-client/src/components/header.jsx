aa8642ce59c4cedbfa120692efd7ae6668bcf8c3
Jelani <jhankins02@gmail.com>
2018-06-03 14:02:22 -0500

    insalled axios, created header component , header renders current location and day

--- react-client/src/components/header.jsx
+++ react-client/src/components/header.jsx
@@ -1,1 +1,14 @@
 import React from 'react';
+
+
+const HappyHourHeader = props => (
+  <div>
+    <h2>Happy Hour App</h2>
+    <h4>Location</h4>
+    <span>{ props.location }</span>
+    <h4>Date</h4>
+    <span>{ props.date }</span>
+  </div>
+);
+
+export default HappyHourHeader;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,21 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import axios from 'axios';
 import HappyHourList from './components/happyHourList.jsx';
+import HappyHourHeader from './components/header.jsx';
 
-
-console.log('IT WORKED');
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      location: 'loading..',
+      date: 'loading..',
       items: [{ description: 'Happy Hour 1' }],
     };
   }
 
   // after the component mounts
   componentDidMount() {
+    // get current location and time
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=AIzaSyDFYgdEUy4CeS5XcKc2G7w3pWjQ7f946DA`)
+          .then((res) => {
+            console.log(res.data.results);
+            this.setState({
+              location: `${res.data.results[0].address_components[2].long_name}, ${res.data.results[0].address_components[3].long_name}`,
+            });
+          })
+          .catch(err => console.log(err));
+        const date = new Date(position.timestamp);
+        this.setState({
+          date: date.toString().substring(0, 16),
+        });
+      },
+      (err) => { console.log(err); },
+    );
     // populate happy Hour list with get request.
     // $.ajax({
     //     url: '/happyHours',
@@ -33,6 +53,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <HappyHourHeader location={this.state.location} date={this.state.date} />
         <h1>Happy Hour List</h1>
         <HappyHourList items={this.state.items} />
       </div>);
