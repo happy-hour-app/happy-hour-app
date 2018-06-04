1acef7806098c8e483e65ea37964d9450531837a
Jelani <jhankins02@gmail.com>
2018-06-04 10:34:10 -0500

    add age and phone number input feilds, sends on post request

--- react-client/src/components/createUserPage.jsx
+++ react-client/src/components/createUserPage.jsx
@@ -14,26 +14,47 @@ class CreateUserPage extends React.Component {
     this.typeFirstName = this.typeFirstName.bind(this);
     this.typeLastName = this.typeLastName.bind(this);
     this.handleCreateUser = this.handleCreateUser.bind(this);
+    this.typeAge = this.typeAge.bind(this);
+    this.typePhoneNumber = this.typePhoneNumber.bind(this);
     // can update to handle input change in here
   }
   // handle on Submit, send post to '/login'
   handleCreateUser(e) {
     console.log(`USEREMAIL: ${this.props.userEmail}`);
     console.log(`PASSWORD: ${this.props.password}`);
-    axios.post('/user', { userEmail: this.props.userEmail, password: this.props.password })
+
+    const newUser = {
+      userEmail: this.props.userEmail,
+      password: this.props.password,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      phoneNumber: this.state.phoneNumber,
+      age: this.state.age,
+    };
+    axios.post('/user', newUser)
       .then((res) => { console.log(res.data); })
       .catch(err => console.log(err));
     e.preventDefault();
   }
-
+  // on change event for last name input
   typeFirstName(event) {
     this.setState({ firstName: event.target.value });
     setTimeout(() => { console.log(this.state.firstName); }, 100);
   }
-  // on change event for password input (Login Page)
+  // on change event for last name input
   typeLastName(event) {
     this.setState({ lastName: event.target.value });
     setTimeout(() => { console.log(this.state.lastName); }, 100);
+  }
+
+  typeAge(event) {
+    this.setState({ age: event.target.value });
+    setTimeout(() => { console.log(this.state.age); }, 100);
+  }
+
+  typePhoneNumber(event) {
+    this.setState({ phoneNumber: event.target.value });
+    setTimeout(() => { console.log(this.state.phoneNumber); }, 100);
   }
 
   render() {
@@ -59,6 +80,15 @@ class CreateUserPage extends React.Component {
         <label>
           Password:
           <input type="text" name="password" value={this.props.password} onChange={this.props.pwchange} />
+        </label>
+        <br />
+        <label>
+        Age:
+          <input type="number" name="age" value={this.state.age} onChange={this.typeAge} />
+        </label>
+        <label>
+        Phone Number:
+          <input type="number" name="phone" value={this.state.phoneNumber} onChange={this.typePhoneNumber} />
         </label>
         <input type="submit" value="Create User" />
         </form>
